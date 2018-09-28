dac83333aa2f0c2f3d7e45aeb30ce2a261b1cf37
HunterYoung <921348203@qq.com>
2018-09-28 14:24:31 +0800

    add addLoadEvent.js

---
+++ addLoadEvent.js
@@ -0,0 +1,14 @@
+/*
+* add function to window.onload
+* */
+function addLoadEvent(func) {
+    var oldOnload = window.onload;
+    if (typeof window.onload !== 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldOnload();
+            func();
+        }
+    }
+}
